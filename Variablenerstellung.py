c34a039311a2ce8a8a7e44fa2aab53d75e8efc91
Ayemann Abid <125467496+aa6id@users.noreply.github.com>
2025-04-22 12:03:00 +0200

    Update Variablenerstellung.py
    Code für die neuen Anforderungen

--- Variablenerstellung.py
+++ Variablenerstellung.py
@@ -6,29 +6,17 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+
 df = pd.read_excel('O:/Dokumente/Spyder-AufG/speech_model_data_baseline (13).xlsx') 
 
 
-y = df['monosyllabic_score']
+df['hearing_impaired_years2'] = df[['hardhearing_years','deaf_years']].max(axis=1)
 
-print(df)
-
-"""
-when deaf_years is true and when hard_hearing_years is true
- then assign to deaf years
- 
-when deaf years is None but hard_hearing_years is true
- then assign to hard_hearing_years
- """
-
-
-df['hearing_impaired_years'] = None
-
-
-for index, row in df.iterrows():
-    if row['deaf_years'] is not None:
-        df.at[index, 'hearing_impaired_years'] = row['deaf_years']
-    elif row['hardhearing_years'] is not None:
-        df.at[index, 'hearing_impaired_years'] = row['hardhearing_years']
-        
-df["hearing_impaired_years"]
+df.to_excel('O:/Dokumente/Spyder-AufG/speech_model_data_baseline (13).xlsx', index = False)
